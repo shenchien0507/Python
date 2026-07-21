1f29358f356498fdecc0bd964e1bc6a696e2b095
shenchien0507 <shenchien0507@gmail.com>
2026-07-21 14:09:31 +0800

    0721課程新增

---
+++ 0721-CH02/variable.py
@@ -0,0 +1,12 @@
+# 變數宣告
+yesNo = 'y' # 宣告yesNo變數，並指定變數值為字串'y'
+passScore, maxScore, minScore = 60, 100, 0 # v同時宣告3個變數，並指定變數值為60,100,0
+power1 = power2 = 100 # 宣告power1和power2變數，變數值都為100
+total = 1.23456E+6 # 先告total變數，並設變數值為浮點數1234560.0
+
+#顯示變數
+print(yesNo)
+print(passScore, maxScore, minScore)
+print(power1, power2)
+print(total)
+del total
