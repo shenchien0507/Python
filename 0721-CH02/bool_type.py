ef466a94f7ccb4e25a979ff7215b859c5fa86417
shenchien0507 <shenchien0507@gmail.com>
2026-07-21 14:49:02 +0800

    0721課程新增-1

---
+++ 0721-CH02/bool_type.py
@@ -0,0 +1,7 @@
+# 布林資料(bool)函式練習
+b=False # 宣告b為布林變數，變數值為Flas
+print(type(b)) # 顯示布林變數b的類別
+print(type(1)) # 顯示整數常值1的類別
+print(bool(1)) # 使用bool函式將1轉成布林值
+print(bool(-1)) # 使用bool函式將-1轉成布林值
+print(bool('Python')) #使用bool函式將'Python'轉成布林值
---
+++ 0721-CH02/float_type.py
@@ -0,0 +1,9 @@
+# 浮點數資料，float函式
+f, i =1.2345, 12345 
+print(type(f)) # 顯示浮點數變值f的類別
+f2=float(i) # 用float函式將整束變數i轉成浮點數
+print(f2)# 顯示f2的變數值
+print(float.is_integer(f)) # 用is_integer()檢查變數(f)是否為整數
+print(float.is_integer(f2)) # 用is_integer()檢查變數(f2)是否為整數
+print(round(f,2)) # 用round()函式將變數f四捨五入到小數二位
+print(round(f)) # 用round()函式將變數f四捨五入到整數
